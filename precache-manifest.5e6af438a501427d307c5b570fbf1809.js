dcc34b369d5d25a366cdc8d9d009ece9373d042d
Roy Amar <royammar030783@gmail.com>
2020-03-12 14:23:56 +0200

    Updates

---
+++ precache-manifest.5e6af438a501427d307c5b570fbf1809.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829649f2ad8c47189cd44efad5ba5058",
+    "url": "/%E2%80%8Bhttps://royammar.github.io/PlayList/index.html"
+  },
+  {
+    "revision": "5b45baf591430722d18c",
+    "url": "/%E2%80%8Bhttps://royammar.github.io/PlayList/static/css/main.def08572.chunk.css"
+  },
+  {
+    "revision": "5d1f3598e8f14d5f13e6",
+    "url": "/%E2%80%8Bhttps://royammar.github.io/PlayList/static/js/2.99e99082.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/%E2%80%8Bhttps://royammar.github.io/PlayList/static/js/2.99e99082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b45baf591430722d18c",
+    "url": "/%E2%80%8Bhttps://royammar.github.io/PlayList/static/js/main.4522fb45.chunk.js"
+  },
+  {
+    "revision": "5e7bea50758acce10b15",
+    "url": "/%E2%80%8Bhttps://royammar.github.io/PlayList/static/js/runtime-main.91af055a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/%E2%80%8Bhttps://royammar.github.io/PlayList/static/media/logo.5d5d9eef.svg"
+  }
+]);
